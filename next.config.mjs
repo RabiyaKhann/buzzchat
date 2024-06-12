51a7e2efc170399b12ca808fc6c185e0aa74f619
RabiyaKhann <90935572+RabiyaKhann@users.noreply.github.com>
2024-06-13 02:47:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-	eslint : {
-		ignoreDuringBuilds: true,
-	},
-	images: {
-		remotePatterns: [
-			{ hostname: "limitless-porpoise-331.convex.cloud" },
-			{ hostname: "oaidalleapiprodscus.blob.core.windows.net" },
-		],
-	},
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /convex\/_generated/,
+      type: 'javascript/esm',
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      { hostname: "limitless-porpoise-331.convex.cloud" },
+      { hostname: "oaidalleapiprodscus.blob.core.windows.net" },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
